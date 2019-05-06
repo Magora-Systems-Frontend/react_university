3c628ea82e4516c57b6fe09bf396a0274bbd1417
Anatoliy Zaporozhets <zaporozhets@magora-systems.com>
2019-05-06 12:25:15 +0700

    [+] #FRONTEND-82 add success popup

--- src/components/EditProfileForm/index.js
+++ src/components/EditProfileForm/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { message } from 'antd';
 import { EditProfileForm } from './form';
 
 export class EditProfile extends React.PureComponent {
   onSubmit = (values) => {
     // eslint-disable-next-line
     console.log(values);
+    window.scrollTo(0, 0);
+    message.success('Profile edited!');
   };
 
   render() {
