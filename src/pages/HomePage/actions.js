82c70b3369edce3a289fef77c9e66db71badb0e6
tsygankov <tsygankov@magora-systems.com>
2019-05-21 12:03:25 +0700

    Frontend-109,110,111,114,115,127,133 - create courses and slider block - done

--- src/pages/HomePage/actions.js
+++ src/pages/HomePage/actions.js
@@ -20,9 +20,35 @@ export function getCourses(title) {
   };
 }
 
+export function getCoursesPopular(title) {
+  const url = `${API_METHODS.COURSES}/${title}`;
+
+  const axios = getAxios();
+
+  let response;
+
+  return async (dispatch) => {
+    try {
+      response = await axios.get(url);
+      dispatch(getCoursesPopularState(response));
+    } catch (error) {
+      return error;
+    }
+
+    return response;
+  };
+}
+
 export function getCoursesState(state) {
   return {
     type: ACTIONS_CONSTANTS_COURSES.COURSES_GET_SUCCESS,
+    payload: state,
+  };
+}
+
+export function getCoursesPopularState(state) {
+  return {
+    type: ACTIONS_CONSTANTS_COURSES.COURSES_POPULAR_GET_SUCCESS,
     payload: state,
   };
 }
