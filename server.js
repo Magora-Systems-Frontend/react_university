c8080f2c0bb025081bc464aba8782f3edab387a8
tsygankov <tsygankov@magora-systems.com>
2019-08-09 10:13:21 +0700

    Frontend-200

---
+++ server.js
@@ -0,0 +1,14 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3001;
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'assets', 'index.html'));
+});
+app.listen(port);
