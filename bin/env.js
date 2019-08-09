7bca3f5144eb33fb94c67270c35b01bb4ece865d
tsygankov <tsygankov@magora-systems.com>
2019-08-09 11:40:28 +0700

    Frontend-200

--- bin/env.js
+++ bin/env.js
@@ -2,7 +2,7 @@
 const defaultEnvVariables = {
   NODE_ENV: 'development',
   HOST: '0.0.0.0',
-  PORT: 8021,
+  PORT: 8020,
   WEB_APP_PREFIX: 'AP'
 };
 const envVariables = Object.keys(process.env).reduce((accumulator, envName) => {
