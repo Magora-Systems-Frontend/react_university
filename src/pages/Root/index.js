30cdcfe2efab5fee2d876d4217ee60ec55a8c43e
tsygankov <tsygankov@magora-systems.com>
2019-10-02 16:38:51 +0700

    Frontend-262

--- src/pages/Root/index.js
+++ src/pages/Root/index.js
@@ -5,20 +5,7 @@ import { ErrorBoundary } from 'components';
 import { routes } from 'routes';
 
 export function Root() {
-  return (
-    <div>
-      <Helmet itleTemplate="%s - Notes" defaultTitle="Notes">
-        <meta name="description" content="Sample Application" />
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous"
-        />
-      </Helmet>
-      <ErrorBoundary>{routes()}</ErrorBoundary>
-    </div>
-  );
+  return <ErrorBoundary>{routes()}</ErrorBoundary>;
 }
 
 Root.propTypes = {};
