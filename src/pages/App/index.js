47283fa96acd7f77bfa7b33613e022d4666bf4ce
Shurumov Aleksei <Shurumov@magora-systems.com>
2019-11-13 10:32:46 +0700

    [+]FRONTEND-300: added tests for Header component

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PT from 'prop-types';
-import { Header } from 'components/Header';
+import Header from 'components/Header';
 import Footer from 'components/Footer';
 
 export class App extends React.PureComponent {
